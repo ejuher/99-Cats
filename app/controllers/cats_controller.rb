72224ee10dba038c0906a1a2e14ca381a1ec94dd
Erich Uher <ejuher@gmail.com>
2014-08-26 11:27:34 -0700

    phase 1 form complete

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -10,11 +10,38 @@ class CatsController < ApplicationController
   end
   
   def new
+    @cat = Cat.new
     render :new
   end
   
   def edit
     @cat = Cat.find(params[:id])
     render :edit
+  end
+  
+  def create
+    @cat = Cat.new(cat_params)
+    
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @cat = Cat.find(params[:id])
+    
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :update
+    end
+  end
+  
+  def cat_params
+    params.require(:cat).permit(
+      :name, :age, :sex, :color, :birth_date, :description
+    )
   end
 end
