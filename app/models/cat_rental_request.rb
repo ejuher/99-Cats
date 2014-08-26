111f6376ea7d3e7d119410b97a29b16f4634c907
Erich Uher <ejuher@gmail.com>
2014-08-26 11:58:58 -0700

    started CatRentalRequest class and added custom validation

---
+++ app/models/cat_rental_request.rb
@@ -0,0 +1,31 @@
+class CatRentalRequest < ActiveRecord::Base
+  validates :cat_id, :start_date, :end_date, :status, presence: true
+  validates :status, inclusion: { in: ['PENDING', 'APPROVED', 'DENIED'] }
+  validate :no_other_rentals_approved_in_range
+  
+
+  
+  def no_other_rentals_approved_in_range
+    unless overlapping_approved_requests.empty?
+      errors[:start_date] << "Rental is already approved for this date range"
+    end
+  end
+  
+  def overlapping_requests
+    range_where = 
+      '(? BETWEEN start_date AND end_date) OR (? BETWEEN start_date AND end_date)'
+    
+    self.class
+      .where(cat_id: cat_id)
+      .where.not(id: id)
+      .where(
+       range_where,
+        start_date,
+        end_date
+      )
+  end
+  
+  def overlapping_approved_requests
+    overlapping_requests.where(status: 'APPROVED')
+  end
+end
